2779c2551137f4ea3be537409ce03576087e0c5e
Ashling McCarthy <ashlingmccarthy02@gmail.com>
2022-06-01 21:53:48 +0100

    I edited the syntax

--- ChickenWings.py
+++ ChickenWings.py
@@ -1,5 +1,5 @@
-print "Hello, I can guess who you are. Just type the adjective that best describes you!"
-if input = ("smelly")
-print "Hello Ash"
-if input = ("handsome")
-print "Hello Bob"
+print ("Hello, I can guess who you are. Just type the adjective that best describes you!")
+if (input == ("smelly")):
+    print ("Hello Ash")
+if (input == ("handsome")):
+    print ("Hello Bob")
--- NewFile.py
+++ NewFile.py
@@ -1,7 +1,7 @@
 print ("Joni Smells")
 joni = 1
 smelli = 1
-if (joni=smelli):
+if (joni == smelli):
   print ("Joni Smells")
   
   
