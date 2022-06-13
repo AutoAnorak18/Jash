e8a3dd5f0258c2847d35952dd62421f463b20bb2
Ashling McCarthy <ashlingmccarthy02@gmail.com>
2022-06-13 19:46:51 +0100

    Trying to download old files

--- Python/ChickenWings.py
+++ Python/ChickenWings.py
@@ -1,6 +1,7 @@
 
-output = input("Hello, I can guess who you are. Just type the adjective that best describes you!")
-if output ==smelly:
+print ("Hello, I can guess who you are. Just type the adjective that best describes you!")
+words = input()
+if (words == "smelly"):
     print ("Hello Ash")
-if output ==handsome:
-    print ("Hello Jon")
+if (words == "handsome"):
+    print ("Hello Bob")
--- Python/HiSiva.py
+++ Python/HiSiva.py
@@ -1,5 +1,9 @@
 if(input ("Type Something") == "jj"):
     print("Hi Siva")
 else:
+<<<<<<< Updated upstream
     print("Hi Everyone")
     
+=======
+    print("Hi Everyone")
+>>>>>>> Stashed changes
